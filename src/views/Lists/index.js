ecbb5581bd4e42766192c6050a0d31ed40ceaf90
Arnar Snaer <arnarsnaer98@gmail.com>
2020-11-23 18:07:14 +0000

    Base setup of list

--- src/views/Lists/index.js
+++ src/views/Lists/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
-import Toolbar from '../../components/Lists/Toolbar';
+import { View, Text } from 'react-native';
+import Header from '../../components/Lists/Header';
+import BottomToolbar from '../../components/Lists/BottomToolbar';
 
 class List extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Toolbar />
+        <Header />
+        <Text>This is where a list should appear</Text>
+        <BottomToolbar />
       </View>
     );
   }
