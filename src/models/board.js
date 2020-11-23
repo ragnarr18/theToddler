baf2fe42765d5f8b931481156433c9aa452d4148
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-11-23 14:53:42 +0000

    Update board.js

--- src/models/board.js
+++ src/models/board.js
@@ -1,1 +1,31 @@
+/*
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text } from 'react-native';
 
+const Board = ({ name, desciptor, photo }) => (
+  <View>
+    <Text>
+      Name:
+      {name}
+    </Text>
+    <Text>
+      Descriptor:
+      { desciptor }
+    </Text>
+    <Text>
+      Thumbnail Photo:
+      { photo }
+    </Text>
+  </View>
+);
+Board.defaultProps = {
+  desciptor: '',
+};
+Board.propTypes = {
+  name: PropTypes.string.isRequired,
+  desciptor: PropTypes.string,
+  photo: PropTypes.string.isRequired,
+};
+export default Board;
+*/
