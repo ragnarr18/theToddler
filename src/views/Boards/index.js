78cb53e1e40d89ac5e3bb2c578797e8d19292d40
ragnarr18 <ragnarr18@skel.ru.is>
2020-11-23 14:01:59 +0000

    boards

---
+++ src/views/Boards/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+const Boards = () => (
+    <View>
+      <Text> Boards<Text/>
+    <View/>
+)
+
+export default Boards;
