eca275563b5582f5b42cfeb5f0cc7a738f548aba
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-11-26 15:24:11 +0000

    Tasks now show up on mobile

--- src/components/Tasks/TaskItem/styles.js
+++ src/components/Tasks/TaskItem/styles.js
@@ -10,7 +10,6 @@ export default StyleSheet.create({
     backgroundColor: '#faa',
   },
   container: {
-    flex: 1,
     backgroundColor: '#aaaaaa',
     borderColor: '#111111',
     borderWidth: 1,
@@ -25,6 +24,7 @@ export default StyleSheet.create({
     padding: 20,
   },
   iconView: {
+    height: 100,
     padding: 20,
     alignItems: 'center',
     flexDirection: 'row',
